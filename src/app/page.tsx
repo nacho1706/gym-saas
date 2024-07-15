c0f69f681935cb2fc97c482ad10f80017dbc2c9a
nacho1706 <ignacioalbarracin15@gmail.com>
2024-07-15 15:37:13 -0300

    Se cambia toda la estructura del proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import BackgroundImage from "@/components/ui/BackgroundImage";
 import { Button } from "@/components/ui/button";
-import InfiniteScroll from "@/components/ui/InfiniteScroller";
+
 
 export default function Home() {
   return (
     <div>
       <section className="relative">
-        <BackgroundImage backgroundImage="url('/gym-photo.jpg')" />
-        <div className="absolute inset-0 bg-black bg-opacity-85"></div>
-        <MaxWidthWrapper className="relative pb-28 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-0 lg:pb-80 xl:gap-x-8 xl:pt-28 z-20">
+        <BackgroundImage backgroundImage="url('/fondo-landing.png')" />
+        <div className="absolute inset-0"></div>
+        <MaxWidthWrapper className="relative pb-28 pt-10 sm:pb-32 lg:gap-x-0 lg:pb-80 xl:gap-x-8 xl:pt-28 z-10">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center">
-              <h1 className="relative w-fit tracking-wider max-sm:tracking-wide text-balance mt-10 font-bold !leading-tight text-white text-6xl md:text-8xl lg:text-9xl lg:text-center font-bebas">
-                ELEVATE YOUR WORKOUT
+              <h1 className="relative w-fit max-sm:tracking-wide text-pretty text-center mt-10 font-bold !leading-tight text-4xl md:text-6xl lg:text-8xl lg:text-clip font-sans">
+              Tu Pedido en <span className="text-white bg-green-500">WhatsApp</span>: Rápido, Fácil y Económico
               </h1>
-              <p className="mt-4 text-xs lg:text-sm lg:pr-10 max-w-prose text-center lg:text-center text-balance md:text-wrap text-white">
+              <p className="mt-4 text-xs lg:text-sm lg:pr-10 max-w-prose lg:text-center text-justify">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Officiis at consequuntur ea, illum voluptatem eos magnam tempora
                 porro, veritatis totam nihil voluptate laborum consectetur
@@ -40,14 +40,14 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
-      <section className="relative w-full bg-black overflow-hidden shadow-[0px_-100px_24px_12px_#0000004d,0px_-60px_30px_0px_#0000004d,0px_100px_24px_12px_#0000004d,0px_60px_30px_0px_#0000004d]">
+      {/* <section className="relative w-full bg-black overflow-hidden shadow-[0px_-100px_24px_12px_#0000004d,0px_-60px_30px_0px_#0000004d,0px_100px_24px_12px_#0000004d,0px_60px_30px_0px_#0000004d]">
         <div className="absolute inset-y-0 left-0 w-16 md:w-60 bg-gradient-to-r from-black via-black/80 to-transparent pointer-events-none z-30"></div>
         <div className="absolute inset-y-0 right-0 w-16 md:w-60 bg-gradient-to-l from-black via-black/80 to-transparent pointer-events-none z-30"></div>
         <div className="h-36 max-sm:h-24 flex items-center justify-center">
           <InfiniteScroll />
         </div>
-      </section>
-      <section className="relative pt-16">
+      </section> */}
+      {/* <section className="relative pt-16">
         <BackgroundImage backgroundImage="url('/fondo_negro.webp')" />
         <div className="md:flex justify-center md:col-start-2">
           <h1 className="relative w-fit tracking-wider max-sm:tracking-wide text-balance mt-10 font-bold !leading-tight text-white text-4xl md:text-6xl lg:text-7xl lg:text-center font-bebas">
@@ -95,7 +95,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
